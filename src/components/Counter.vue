1ef8290b371370e91098071756489420be6eecec
Pablo Contreras <pacg1991@gmail.com>
2023-04-06 12:50:19 -0500

    Installed: Pinia
    - [COMPONENTS] Pinia
    - [TYPE] Add

---
+++ src/components/Counter.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+
+const counter = useCounterStore()
+
+counter.count++
+// with autocompletion ✨
+counter.$patch({ count: counter.count + 1 })
+// or using an action instead
+counter.increment()
+</script>
+
+<template>
+  <div>
+    <button type="button" @click="counter.increment()">count is: {{ counter.count }}</button>
+  </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import Vue from 'vue';
-import './style.css'
-import App from './App.vue';
+import { createPinia, PiniaVuePlugin } from "pinia";
+import "./style.css";
+import App from "./App.vue";
+
+Vue.use(PiniaVuePlugin);
+const pinia = createPinia();
 
 new Vue({
   render: (h) => h(App),
-}).$mount('#app');
+  pinia,
+}).$mount("#app");
