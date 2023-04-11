11b12e0d1d780c60b77ca89d0b14356f40c0ebf1
Pablo Contreras <pacg1991@gmail.com>
2023-04-11 11:25:06 -0500

    vite.config eslint
    - [COMPONENTS] Config
    - [TYPE] Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
+// import eslintPlugin from 'vite-plugin-eslint'
 import vue from '@vitejs/plugin-vue2'
 import { resolve as pathResolve } from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // eslintPlugin(),
+  ],
   resolve: {
     alias: [
       {
