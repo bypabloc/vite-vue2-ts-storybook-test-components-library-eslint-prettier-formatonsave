08b3e2bb5db41553e7ff95c7d88af56660bbd525
Pablo Contreras <pacg1991@gmail.com>
2023-04-12 14:11:13 -0500

    Vite build option mode development and library
    - [COMPONENTS] Config
    - [TYPE] Update

--- vite.config.ts
+++ vite.config.ts
@@ -5,65 +5,109 @@ import { resolve as pathResolve } from 'path'
 import { configDefaults } from 'vitest/config'
 import vue from '@vitejs/plugin-vue2'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    // eslintPlugin(),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: /^@\/(.+)/,
-        replacement: pathResolve(pathResolve(__dirname), 'src') + '/$1',
-      },
-    ],
-  },
-  server: {
-    port: 3000,
-  },
-  test: {
-    environment: 'happy-dom',
-    globals: true,
-    exclude: [
-      ...configDefaults.exclude,
-      '**/node_modules/**',
-      '**/dist/**',
-      '**/build/**',
-      '**/lib/**',
-      '**/public/**',
-      '**/.{idea,git,cache,output,temp}/**',
-      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
-    ],
-    include: [
-      ...configDefaults.include,
-      './src/**/*.{spec,test}.{js,jsx,ts,tsx,vue}',
-    ],
-  },
 
-  build: {
-    // Output compiled files to /dist.
-    cssCodeSplit: true,
-    outDir: './lib',
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: pathResolve(__dirname, 'src/rollup.ts'),
-      name: 'MyLib',
-      // the proper extensions will be added
-      fileName: 'my-lib',
-      formats: ['es', 'cjs', 'umd'],
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
+// https://vitejs.dev/config/#build-lib
+// https://vitejs.dev/guide/env-and-mode.html
+// https://vitejs.dev/config/build-options.html
+
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  const modes = {
+    default: () => ({
+      plugins: [
+        vue(),
+        // eslintPlugin(),
+      ],
+      resolve: {
+        alias: [
+          {
+            find: /^@\/(.+)/,
+            replacement: pathResolve(pathResolve(__dirname), 'src') + '/$1',
+          },
+        ],
+      },
+      server: {
+        port: 3000,
+      },
+      test: {
+        environment: 'happy-dom',
+        globals: true,
+        exclude: [
+          ...configDefaults.exclude,
+          '**/node_modules/**',
+          '**/dist/**',
+          '**/build/**',
+          '**/lib/**',
+          '**/public/**',
+          '**/.{idea,git,cache,output,temp}/**',
+          '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        ],
+        include: [
+          ...configDefaults.include,
+          './src/**/*.{spec,test}.{js,jsx,ts,tsx,vue}',
+        ],
+      },
+    }),
+    library: () => ({
+      plugins: [
+        vue(),
+        // eslintPlugin(),
+      ],
+      resolve: {
+        alias: [
+          {
+            find: /^@\/(.+)/,
+            replacement: pathResolve(pathResolve(__dirname), 'src') + '/$1',
+          },
+        ],
+      },
+      server: {
+        port: 3000,
+      },
+      test: {
+        environment: 'happy-dom',
+        globals: true,
+        exclude: [
+          ...configDefaults.exclude,
+          '**/node_modules/**',
+          '**/dist/**',
+          '**/build/**',
+          '**/lib/**',
+          '**/public/**',
+          '**/.{idea,git,cache,output,temp}/**',
+          '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        ],
+        include: [
+          ...configDefaults.include,
+          './src/**/*.{spec,test}.{js,jsx,ts,tsx,vue}',
+        ],
+      },
+      build: {
+        // Output compiled files to /dist.
+        cssCodeSplit: true,
+        outDir: './lib',
+        lib: {
+          // Could also be a dictionary or array of multiple entry points
+          entry: pathResolve(__dirname, 'src/rollup.ts'),
+          name: 'MyLib',
+          // the proper extensions will be added
+          fileName: 'my-lib',
+          formats: ['es', 'cjs', 'umd'],
+        },
+        rollupOptions: {
+          // make sure to externalize deps that shouldn't be bundled
+          // into your library
+          external: ['vue'],
+          output: {
+            // Provide global variables to use in the UMD build
+            // for externalized deps
+            globals: {
+              vue: 'Vue',
+            },
+          },
         },
       },
-    },
-  },
+    }),
+  }
+  const modeFn = modes[mode] || modes.default
+  return modeFn()
 })
